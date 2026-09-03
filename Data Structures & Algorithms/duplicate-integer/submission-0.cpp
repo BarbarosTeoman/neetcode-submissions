1795e0bcda8fc08eec17d9facf9ccfec172bc59a
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2026-09-03 16:20:08 +0200

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        for (int x : nums) {
+            if (seen.count(x)) {
+                return true;
+            }
+            seen.insert(x);
+        }
+        return false;
+    }
+};
