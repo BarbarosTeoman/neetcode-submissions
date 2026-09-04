225c9e3c458fe1fed4b1ecbc224848669955c21e
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2026-09-04 15:18:20 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int first = 0;
+        int second = s.size()-1;
+
+        while (first < second)
+        {
+            while (first < second && !isalnum(s[first]))
+            {
+                first++;
+                cout << "1";
+            }
+            while (first < second && !isalnum(s[second]))
+            {
+                second--;
+                cout << "2";
+            }
+            if (tolower(s[first]) != tolower(s[second]))
+            {
+                return false;
+            }
+
+            first++;
+            second--;
+        }
+        return true;
+    }
+};
