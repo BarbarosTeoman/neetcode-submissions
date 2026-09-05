7b4265b8d034acc23742fa7c252a42db76b69ca8
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2026-09-05 16:05:42 +0200

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        unordered_set<ListNode*> seenNodes;
+
+        while (head != nullptr)
+        {
+            if (seenNodes.count(head) == 0)
+            {
+                seenNodes.insert(head);
+            } 
+            else 
+            {
+                return true;
+            }
+            head = head->next;
+        }
+        return false;
+    }
+};
